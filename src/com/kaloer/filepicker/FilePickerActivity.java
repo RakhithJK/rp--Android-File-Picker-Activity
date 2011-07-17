7086422b0d23018fd91aa07d532519dacc19e848
Peinthor Rene <peinthor@gmail.com>
2011-07-17 17:08:57 +0200

    Add a save file mode, it can be toggled by the EXTRA_SAVE_FILE boolean intent extra

--- src/com/kaloer/filepicker/FilePickerActivity.java
+++ src/com/kaloer/filepicker/FilePickerActivity.java
@@ -23,9 +23,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import com.kaloer.filepicker.R;
-
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -33,18 +31,30 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class FilePickerActivity extends ListActivity {
-	
+public class FilePickerActivity extends Activity {
+
 	/**
 	 * The file path
 	 */
 	public final static String EXTRA_FILE_PATH = "file_path";
-	
+
+	/**
+	 * Save file mode
+	 */
+	public final static String EXTRA_SAVE_FILE = "save_file";
+
 	/**
 	 * Sets whether hidden files should be visible in the list or not
 	 */
@@ -54,88 +64,154 @@ public class FilePickerActivity extends ListActivity {
 	 * The allowed file extensions in an ArrayList of Strings
 	 */
 	public final static String EXTRA_ACCEPTED_FILE_EXTENSIONS = "accepted_file_extensions";
-	
+
 	/**
-	 * The initial directory which will be used if no directory has been sent with the intent 
+	 * The initial directory which will be used if no directory has been sent
+	 * with the intent
 	 */
 	private final static String DEFAULT_INITIAL_DIRECTORY = "/";
-	
+
 	protected File mDirectory;
 	protected ArrayList<File> mFiles;
+	protected LinearLayout mLinearLayout;
+	protected ListView mFileList;
+	protected TextView mTVCurrentFolder;
+	protected Button mButCancel;
+	protected EditText mEditFileName;
 	protected FilePickerListAdapter mAdapter;
 	protected boolean mShowHiddenFiles = false;
 	protected String[] acceptedFileExtensions;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
+		setContentView(R.layout.file_picker_view);
+
+		// Main linear layout
+		mLinearLayout = (LinearLayout) findViewById(R.id.file_picker_layout);
+
+		// Cancel button
+		mButCancel = (Button) findViewById(R.id.butCancel);
+		mButCancel.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				setResult(RESULT_CANCELED);
+				finish();
+			}
+		});
+
 		// Set the view to be shown if the list is empty
-		LayoutInflater inflator = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View emptyView = inflator.inflate(R.layout.file_picker_empty_view, null);
-		((ViewGroup)getListView().getParent()).addView(emptyView);
-		getListView().setEmptyView(emptyView);
-		
+		mFileList = (ListView) findViewById(R.id.lvFilePicker);
+		LayoutInflater inflator = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View emptyView = inflator
+				.inflate(R.layout.file_picker_empty_view, null);
+		mFileList.setEmptyView(emptyView);
+
+		mFileList.setOnItemClickListener(new FilePickListClickListener());
+
+		mTVCurrentFolder = (TextView) findViewById(R.id.tvCurrentFolder);
+
 		// Set initial directory
 		mDirectory = new File(DEFAULT_INITIAL_DIRECTORY);
-		
+		mTVCurrentFolder.setText(mDirectory.getAbsolutePath());
+
 		// Initialize the ArrayList
 		mFiles = new ArrayList<File>();
-		
+
 		// Set the ListAdapter
 		mAdapter = new FilePickerListAdapter(this, mFiles);
-		setListAdapter(mAdapter);
-		
+		mFileList.setAdapter(mAdapter);
+
 		// Initialize the extensions array to allow any file extensions
 		acceptedFileExtensions = new String[] {};
-		
+
 		// Get intent extras
-		if(getIntent().hasExtra(EXTRA_FILE_PATH)) {
+		if (getIntent().hasExtra(EXTRA_FILE_PATH)) {
 			mDirectory = new File(getIntent().getStringExtra(EXTRA_FILE_PATH));
 		}
-		if(getIntent().hasExtra(EXTRA_SHOW_HIDDEN_FILES)) {
-			mShowHiddenFiles = getIntent().getBooleanExtra(EXTRA_SHOW_HIDDEN_FILES, false);
+		if (getIntent().hasExtra(EXTRA_SHOW_HIDDEN_FILES)) {
+			mShowHiddenFiles = getIntent().getBooleanExtra(
+					EXTRA_SHOW_HIDDEN_FILES, false);
 		}
-		if(getIntent().hasExtra(EXTRA_ACCEPTED_FILE_EXTENSIONS)) {
-			ArrayList<String> collection = getIntent().getStringArrayListExtra(EXTRA_ACCEPTED_FILE_EXTENSIONS);
-			acceptedFileExtensions = (String[]) collection.toArray(new String[collection.size()]);
+		if (getIntent().hasExtra(EXTRA_ACCEPTED_FILE_EXTENSIONS)) {
+			ArrayList<String> collection = getIntent().getStringArrayListExtra(
+					EXTRA_ACCEPTED_FILE_EXTENSIONS);
+			acceptedFileExtensions = (String[]) collection
+					.toArray(new String[collection.size()]);
+		}
+
+		if (getIntent().hasExtra(EXTRA_SAVE_FILE)) {
+			Button butOk = new Button(this);
+			butOk.setId(666);
+			mEditFileName = new EditText(this);
+			LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+					LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT, 1);
+			mEditFileName.setLayoutParams(lp);
+			mEditFileName.setSingleLine();
+			mEditFileName.setImeOptions(EditorInfo.IME_ACTION_NEXT);
+			mEditFileName.setNextFocusDownId(butOk.getId());
+
+			butOk.setText(getString(android.R.string.ok));
+			butOk.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Intent extra = new Intent();
+					String sFileName = mEditFileName.getText().toString().trim();
+					if(mEditFileName.length() > 0) {
+						extra.putExtra(
+								EXTRA_FILE_PATH,
+								mDirectory.getAbsolutePath() + "/" + sFileName);
+						setResult(RESULT_OK, extra);
+						finish();
+					}
+				}
+			});
+
+			LinearLayout layoutFilename = new LinearLayout(this);
+			layoutFilename.setOrientation(LinearLayout.HORIZONTAL);
+			layoutFilename.addView(mEditFileName);
+			layoutFilename.addView(butOk);
+
+			mLinearLayout.addView(layoutFilename, 2);
 		}
 	}
-	
+
 	@Override
 	protected void onResume() {
 		refreshFilesList();
 		super.onResume();
 	}
-	
+
 	/**
 	 * Updates the list view to the current directory
 	 */
 	protected void refreshFilesList() {
 		// Clear the files ArrayList
 		mFiles.clear();
-		
+		mTVCurrentFolder.setText(" " + mDirectory.getAbsolutePath());
+
 		// Set the extension file filter
-		ExtensionFilenameFilter filter = new ExtensionFilenameFilter(acceptedFileExtensions);
-		
+		ExtensionFilenameFilter filter = new ExtensionFilenameFilter(
+				acceptedFileExtensions);
+
 		// Get the files in the directory
 		File[] files = mDirectory.listFiles(filter);
-		if(files != null && files.length > 0) {
-			for(File f : files) {
-				if(f.isHidden() && !mShowHiddenFiles) {
+		if (files != null && files.length > 0) {
+			for (File f : files) {
+				if (f.isHidden() && !mShowHiddenFiles) {
 					// Don't add the file
 					continue;
 				}
-				
+
 				// Add the file the ArrayAdapter
 				mFiles.add(f);
 			}
-			
+
 			Collections.sort(mFiles, new FileComparator());
 		}
 		mAdapter.notifyDataSetChanged();
 	}
-	
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
@@ -149,105 +225,112 @@ public class FilePickerActivity extends ListActivity {
 
 		return super.onKeyDown(keyCode, event);
 	}
-	
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		File newFile = (File)l.getItemAtPosition(position);
-		
-		if(newFile.isFile()) {
-			// Set result
-			Intent extra = new Intent();
-			extra.putExtra(EXTRA_FILE_PATH, newFile.getAbsolutePath());
-			setResult(RESULT_OK, extra);
-			// Finish the activity
-			finish();
-		} else {
-			mDirectory = newFile;
-			// Update the files list
-			refreshFilesList();
+
+	private class FilePickListClickListener implements OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+			File newFile = (File) l.getItemAtPosition(position);
+
+			if (newFile.isFile()) {
+				// Set result
+				Intent extra = new Intent();
+				extra.putExtra(EXTRA_FILE_PATH, newFile.getAbsolutePath());
+				setResult(RESULT_OK, extra);
+				// Finish the activity
+				finish();
+			} else {
+				mDirectory = newFile;
+				// Update the files list
+				refreshFilesList();
+			}
+
+			// super.onListItemClick(l, v, position, id);
 		}
-		
-		super.onListItemClick(l, v, position, id);
 	}
-	
+
 	private class FilePickerListAdapter extends ArrayAdapter<File> {
-		
+
 		private List<File> mObjects;
-		
+
 		public FilePickerListAdapter(Context context, List<File> objects) {
-			super(context, R.layout.file_picker_list_item, android.R.id.text1, objects);
+			super(context, R.layout.file_picker_list_item, android.R.id.text1,
+					objects);
 			mObjects = objects;
 		}
-		
+
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			
+
 			View row = null;
-			
-			if(convertView == null) { 
-				LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				row = inflater.inflate(R.layout.file_picker_list_item, parent, false);
+
+			if (convertView == null) {
+				LayoutInflater inflater = (LayoutInflater) getContext()
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				row = inflater.inflate(R.layout.file_picker_list_item, parent,
+						false);
 			} else {
 				row = convertView;
 			}
 
 			File object = mObjects.get(position);
 
-			ImageView imageView = (ImageView)row.findViewById(R.id.file_picker_image);
-			TextView textView = (TextView)row.findViewById(R.id.file_picker_text);
+			ImageView imageView = (ImageView) row
+					.findViewById(R.id.file_picker_image);
+			TextView textView = (TextView) row
+					.findViewById(R.id.file_picker_text);
 			// Set single line
 			textView.setSingleLine(true);
-			
+
 			textView.setText(object.getName());
-			if(object.isFile()) {
+			if (object.isFile()) {
 				// Show the file icon
 				imageView.setImageResource(R.drawable.file);
 			} else {
 				// Show the folder icon
 				imageView.setImageResource(R.drawable.folder);
 			}
-			
+
 			return row;
 		}
 
 	}
-	
+
 	private class FileComparator implements Comparator<File> {
-	    @Override
-	    public int compare(File f1, File f2) {
-	    	if(f1 == f2) {
-	    		return 0;
-	    	}
-	    	if(f1.isDirectory() && f2.isFile()) {
-	        	// Show directories above files
-	        	return -1;
-	        }
-	    	if(f1.isFile() && f2.isDirectory()) {
-	        	// Show files below directories
-	        	return 1;
-	        }
-	    	// Sort the directories alphabetically
-	        return f1.getName().compareToIgnoreCase(f2.getName());
-	    }
+		@Override
+		public int compare(File f1, File f2) {
+			if (f1 == f2) {
+				return 0;
+			}
+			if (f1.isDirectory() && f2.isFile()) {
+				// Show directories above files
+				return -1;
+			}
+			if (f1.isFile() && f2.isDirectory()) {
+				// Show files below directories
+				return 1;
+			}
+			// Sort the directories alphabetically
+			return f1.getName().compareToIgnoreCase(f2.getName());
+		}
 	}
-	
+
 	private class ExtensionFilenameFilter implements FilenameFilter {
 		private String[] mExtensions;
-		
+
 		public ExtensionFilenameFilter(String[] extensions) {
 			super();
 			mExtensions = extensions;
 		}
-		
+
 		@Override
 		public boolean accept(File dir, String filename) {
-			if(new File(dir, filename).isDirectory()) {
+			if (new File(dir, filename).isDirectory()) {
 				// Accept all directory names
 				return true;
 			}
-			if(mExtensions != null && mExtensions.length > 0) {
-				for(int i = 0; i < mExtensions.length; i++) {
-					if(filename.endsWith(mExtensions[i])) {
+			if (mExtensions != null && mExtensions.length > 0) {
+				for (int i = 0; i < mExtensions.length; i++) {
+					if (filename.endsWith(mExtensions[i])) {
 						// The filename ends with the extension
 						return true;
 					}
